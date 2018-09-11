6c36e33ead0cde27433ac82e8425d1384cc5ff30
yourgirlfriend <cmsmith2067@gmail.com>
2018-09-11 08:22:07 -0700

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+
+public class Welcome {
+	public static void main(String[] args)
+	{
+		System.out.println("(\\__/)");
+		System.out.println("(0^.^)");
+		System.out.println("z(_(\")(\")");
+	}
+
+}
